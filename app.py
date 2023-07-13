086654c300830d6754e7a066120b2183cc35fce0
Ole Voß <ole.voss@posteo.de>
2023-07-14 00:50:23 +0200

    add api support

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import customtkinter
 from PIL import Image
-from threading import Thread
+
+from inventree.part import Part
+from inventree.stock import StockItem, StockLocation
 
 
 class App(customtkinter.CTk):
-    def __init__(self):
+    def __init__(self, api):
         super().__init__()
         self.attributes("-fullscreen", False)
+
+        self.api = api
 
         self.title("inventree scanner")
         self.geometry("800x480")
@@ -20,9 +24,26 @@ class App(customtkinter.CTk):
         self.part_frame.grid(row=0, column=0, padx=10, pady=10, sticky="news")
         self.part_frame.configure(fg_color="skyblue", bg_color="lightgray")
 
+        self.bind("<Key>", self.get_input)
+
+        self.part_code = ""
+
+    def get_input(self, event):
+        if event.char in "0123456789":
+            self.part_code += event.char
+        else:
+            self.part_frame.set_part_details(self.part_code)
+            self.part_code = ""
+            item_data = self.api.get("/stock/82")
+            self.set_new_stock_item(item_data)
+
+    def set_new_stock_item(self, item_data):
+        print(item_data)
+
 
 class PartDisplay(customtkinter.CTkFrame):
     def __init__(self, master):
+
         super().__init__(master)
 
         self.rowconfigure(0, weight=4, uniform="top_down")
@@ -37,19 +58,13 @@ class PartDisplay(customtkinter.CTkFrame):
         self.info_frame.grid(row=0, column=1, padx=(
             5, 10), pady=(10, 5), sticky="news")
 
-        self.data = "test"
         self.info_data = customtkinter.CTkLabel(
-            self.info_frame, text=self.data)
+            self.info_frame)
         self.info_data.grid()
 
         self.util_frame = customtkinter.CTkFrame(self)
         self.util_frame.grid(row=1, padx=10, pady=(5, 10),
                              sticky="ews", columnspan=2)
 
-        master.bind("<Key>", self.get_input)
-
-        print("rdy")
-
-    def get_input(self, event):
-        self.info_data.configure(text=event.char)
-        print("event")
+    def set_part_details(self, id):
+        self.info_data.configure(text=id)
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from app import App
 
-app = App()
+from inventree.api import InvenTreeAPI
+
+
+URL = "https://demo.inventree.org"
+USER = "engineer"
+PASSWORD = "partsonly"
+
+api = InvenTreeAPI(host=URL, username=USER, password=PASSWORD)
+
+app = App(api)
 app.mainloop()
